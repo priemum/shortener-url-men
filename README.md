21cfd9f576ea1b64b784aec0a74dc8d03534991e
Gabriel Vazquez <contacto@gabrielvazquez.com>
2020-12-18 23:10:12 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# shortener-url-men
+
+Shortener url small app that stored the links into a mongo collection and keep a record of how many times have been clicked each one. ExpressJS is used for the server and EJS as a template engine.
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+//Homepage recover all the urls stored
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+//Post from the form, save a new url
+app.post("/shortUrl", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
+
+//Redirection to the url clicked
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) {
+    return res.sendStatus(404);
+  }
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 5000);
